1deb6cb7a4a2ff50a1389d7500201ab7c2a48a81
Victor Johan <Vicjohan23@gmail.com>
2020-05-21 03:14:25 -0400

    SE CREO EL WPF DE REGISTRO

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,9 +15,7 @@ using System.Windows.Shapes;
 
 namespace Registro
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+   
     public partial class MainWindow : Window
     {
         public MainWindow()
